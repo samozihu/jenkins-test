457f395f7162a2d54481cf061a1c9924e4500161
samozihu <samozihu@qq.com>
2015-06-15 21:28:26 +0800

    test jenkins

---
+++ foo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+def divide(num1, num2):
+    '''
+    the function to calculate the division
+    '''
+    return num1 / num2
---
+++ test_foo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import unittest
+import foo
+
+class SimpleTest(unittest.TestCase):
+    '''Test the funtion of division
+    '''
+
+    def test1(self):
+        self.assertEqual(foo.divide(2, 2), 1)
+
+    def test2(self):
+        self.assertEqual(foo.divide(0, 1), 1)
+
+if __name__ == "__main__":
+    unittest.main()
